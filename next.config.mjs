a686f399fe7c957848b0ea1e641b21bbd05e95c5
Amitupadhyay878 <amitupadhyay878@gmail.com>
2025-09-11 13:44:06 +0530

    add headers in next cofig file

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,63 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
+  robotsTxt: true,
+  
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'x-robots-tag',
+            value: 'index, follow',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; img-src 'self' data:; font-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; frame-src 'self';",
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(), microphone=(), camera=()',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'Feature-Policy',
+            value: 'geolocation "self"; microphone "self"; camera "self";',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self';",
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
